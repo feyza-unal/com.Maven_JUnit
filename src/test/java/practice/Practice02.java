7a35c3205af35eb5203f520f6d126086213c3d3e
feyza-unal <feyzaunal95@gmail.com>
2022-12-27 22:59:25 +0300

    practice

---
+++ src/test/java/practice/Practice02.java
@@ -0,0 +1,48 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Practice02 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+//      go to url : https://www.techlistic.com/p/selenium-practice-form.html
+        driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+
+//      fill the firstname
+//      fill the lastname
+//      check the gender
+//      check the experience
+//      fill the date
+//      choose your profession -> Automation Tester
+//      choose your tool -> Selenium Webdriver
+//      choose your continent -> Antartica
+//      choose your command  -> Browser Commands
+//      click submit button
+
+        WebElement firstName = driver.findElement(By.xpath("//*[@name='firstname']"));
+        firstName.sendKeys("Erol",
+                Keys.TAB,"Evren",
+                Keys.TAB,Keys.ARROW_RIGHT, //--> gender(female)
+                Keys.TAB,Keys.ARROW_RIGHT,Keys.ARROW_RIGHT,Keys.ARROW_RIGHT, //--> experience(4)
+                Keys.TAB,"12/12/2022", //-->date
+                Keys.TAB,Keys.TAB,Keys.SPACE, //--> profession(automation tester)
+                Keys.TAB,Keys.TAB,Keys.TAB,Keys.SPACE, //-->Tool(Selenium)
+                Keys.TAB,"Antartica", //-->continent(Antartica)
+                Keys.TAB,Keys.ARROW_DOWN, //-->command (Browser)
+                Keys.TAB,Keys.TAB,Keys.TAB,Keys.ENTER   //-->submit button
+        );  // --> taba basakarak bir sonrakine gectik
+
+
+    }
+}
---
+++ src/test/java/practice/Practice03.java
@@ -0,0 +1,50 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Practice03 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+//      Navigate to  https://testpages.herokuapp.com/styled/index.html
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+
+//      Click on  Calculate under Micro Apps
+        driver.findElement(By.id("calculatetest")).click();
+
+//      Type any number in the first input
+        driver.findElement(By.id("number1")).sendKeys("15", Keys.ENTER);
+
+//      Type any number in the second input
+        driver.findElement(By.id("number2")).sendKeys("10", Keys.ENTER);
+
+//      Click on Calculate
+        driver.findElement(By.id("calculate")).click();
+
+//      Print the result
+        WebElement result = driver.findElement(By.xpath("//*[@id='answer']"));
+        System.out.println(result.getText());
+
+// 2.yol
+//        driver.get("https://testpages.herokuapp.com/styled/index.html");
+//        driver.findElement(By.id("calculatetest")).click();
+//        WebElement firstName = driver.findElement(By.id("number1"));
+//        firstName.sendKeys("30", Keys.TAB,Keys.TAB,"30",Keys.ENTER);
+//        WebElement sonuc = driver.findElement(By.xpath("//*[@ID='answer']"));
+//        System.out.println("sonuc.getText() = " + sonuc.getText());
+
+//      close driver
+        driver.close();
+
+    }
+}
