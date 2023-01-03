4015002fd7e28741293ee7d391ebb2c7b163f725
feyza-unal <feyzaunal95@gmail.com>
2023-01-04 01:10:02 +0300

    practice

--- src/test/java/classworks/TestCase09.java
+++ src/test/java/classworks/TestCase09.java
@@ -14,21 +14,23 @@ public class TestCase09 extends TestBase {
         driver.get("http://demo.guru99.com/test/guru99home/");
 
 //      sayfadaki iframe sayısını bulunuz.
-        List<WebElement> iframeList = driver.findElements(By.tagName("<iframe"));
+        List<WebElement> iframeList = driver.findElements(By.xpath("//iframe"));
         int iframeSayisi = iframeList.size();
         System.out.println(iframeSayisi);
 
 //      ilk iframe'deki (Youtube) play butonuna tıklayınız.
-//        driver.switchTo().frame(2);
-//        driver.findElement(By.xpath("//*[@class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
-//        Thread.sleep(3000);
+        WebElement frame = driver.findElement(By.xpath("//*[@wmode='transparent']"));
+        driver.switchTo().frame(frame);
+        driver.findElement(By.xpath("//*[@aria-label='Oynat']")).click();
+
 
 //      ilk iframe'den çıkıp ana sayfaya dönünüz
-        driver.switchTo().defaultContent();
+        //driver.switchTo().defaultContent(); yada
+        driver.navigate().refresh();
 
 //      ikinci iframe'deki (Jmeter Made Easy) linke (https://www.guru99.com/live-selenium-project.html) tıklayınız
-//        driver.switchTo().frame(By
-//        driver.findElement(By.xpath("(//*[@class='vsc-initialized'])[2]")).click();
-//        Thread.sleep(3000);
+        WebElement iframe2 = driver.findElement(By.xpath("//*[@id='a077aa5e']"));
+        driver.switchTo().frame(iframe2);
+        driver.findElement(By.xpath("//*[@src='Jmeter720.png']")).click();
     }
 }
